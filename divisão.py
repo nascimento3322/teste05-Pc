61e22b8dac7738576b771b1636f05f108469fd28
nascimento3322 <vn62511@gmail.com>
2025-05-10 10:10:56 -0300

    Dividindo

---
+++ divisão.py
@@ -0,0 +1,5 @@
+#Divisão
+g = int(input("Fale um número para começar a divisão: "))
+h = int(input("Complete o calculo adicionando mais um número: "))
+divisão = g / h 
+print(f"A divisão final desse calculo é: {divisão}")
