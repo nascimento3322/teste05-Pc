0db4717ec97c35a43554161797c8df6a452bbb6c
nascimento3322 <vn62511@gmail.com>
2025-05-10 10:20:14 -0300

    Potencializando

---
+++ potenciação.py
@@ -0,0 +1,5 @@
+#Potenciação
+i = int(input("Digite um número: "))
+j = int(input("Digite mais um número para concluir o potenciação: "))
+potenciação = i ** j 
+print(f"O resultado final da potenciação é: {potenciação}")
