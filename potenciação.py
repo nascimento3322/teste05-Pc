f8cc20f8b9d008db19262142edb8e8d4c98df355
nascimento3322 <vn62511@gmail.com>
2025-05-10 10:36:45 -0300

    Update potenciação.py

--- potenciação.py
+++ potenciação.py
@@ -1,5 +1,5 @@
 #Potenciação
-i = int(input("Digite um número: "))
-j = int(input("Digite mais um número para concluir o potenciação: "))
+y = int(input("Digite um número: "))
+z = int(input("Digite mais um número para concluir o potenciação: "))
 potenciação = i ** j 
 print(f"O resultado final da potenciação é: {potenciação}")
